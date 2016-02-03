6197252617e377ce9ab2e9c3506bda34de9bb9a7
bcui <bcui@me.com>
2016-02-03 08:21:28 +0800

    service

--- app/Services/todoProvider.ts
+++ app/Services/todoProvider.ts
@@ -2,10 +2,19 @@
  * Created by bcui on 2/1/16.
  */
 import {Injectable} from "angular2/core"
-import {todoItem} from "../Components/todoItem/todo.item"
+import {todoItem} from "../Models/todoItem/todo.item"
 
 @Injectable()
 
 export class todoProvider{
+    public getToDos() {
+        return [
+          {name : "To Do 1", status: "started"},  
+          {name : "To Do 2", status: "started"},
+          {name : "To Do 3", status: "completed"},
+          {name : "To Do 4", status: "started"}
+        ];
+    }
+    
 }
 
